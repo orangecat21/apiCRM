99bc3063af39e2d207f45b1ff57b6e5df947c0dd
orangefruit21 <borovskih.05021999@gmail.com>
2020-04-03 00:49:30 +0300

    Almost complete backend

--- controllers/category.js
+++ controllers/category.js
@@ -34,16 +34,34 @@ module.exports.remove = async function (req, res) {
 }
 
 module.exports.create = async function (req, res) {
+    const category = new Category({
+        name: req.body.name,
+        user: req.user.id,
+        imageSrc: req.file ? req.file.path : ''
+    })
     try {
-
+        await category.save();
+        res.status(201).json(category);
     } catch (error) {
         errorHandler(error);
     }
 }
 
 module.exports.update = async function (req, res) {
-    try {
+    const updated = {
+        name: req.body.name
+    };
 
+    if (req.file){
+        updated.imageSrc = req.file.path;
+    }
+    try {
+        const category = await Category.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: updated},
+            {new: true}
+        );
+        res.status(200).json(category);
     } catch (error) {
         errorHandler(error);
     }
--- middleware/upload.js
+++ middleware/upload.js
@@ -6,10 +6,25 @@ const storage = multer.diskStorage({
         cb(null, 'uploads/')
     },
     filename(req, file, cb){
-
+        const date = moment().format('DDMMYYYY-HHmmss_SSS');
+        cb(null, `${date}-${file.originalname}`);
     }
 });
 
-module.exports = multer({
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpeg'){
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
 
+const limits = {
+    fileSize: 1024 * 1024 * 5
+}
+
+module.exports = multer({
+    storage,
+    fileFilter,
+    limits
 })
